dfe31d78c95edff0843b4bac8b30c8b1a03e5421
valeriehase <valerie.hase@web.de>
2024-03-18 14:54:43 +0100

    updated prisma flowchart

--- prisma/PRISMA.R
+++ prisma/PRISMA.R
@@ -1,7 +1,7 @@
 library(PRISMAstatement)
 
 flow.chart <- flow_exclusions(
-  incl_counts = c(17255, 5252, 1002, 500),
+  incl_counts = c(17255, 5252, 1002, 554),
   total_label = "Hits in Scopus and WoS",
   incl_labels = c("Population of deduplicated articles", "Stratified sample of abstracts", "Sample of relevant abstracts"),
   excl_labels = c("Removal of duplicates", "Removal via sampling", "Removal of irrelevant abstracts"),
