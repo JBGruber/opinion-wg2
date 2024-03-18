7851dad48c492b2c28cd1a063918bdc49417438b
valeriehase <valerie.hase@web.de>
2024-03-18 14:52:04 +0100

    Updated folder (single folder for abstract coding, reli tests, cleaning of first batch of 1,002 coded abstracts); updated code for cleaning first batch of 1,002 coded abstracts

--- coding-abstracts/abstract-coding.R
+++ coding-abstracts/abstract-coding.R
@@ -1,4 +1,7 @@
-### Script for abstract coding - reliability, sample for coding, results of coding ###
+### Script for abstract coding - reliability, sample for coding, results of coding 1 (N = 1,002) ###
+
+#all relevant data for reproducibility in this Drive folder: https://drive.google.com/drive/u/1/folders/1KSkZW_IoIAjcSiaTGmjHdhZetNV41_l6 
+#(except for sample of abstracts, see specific link below)
 
 library(here)
 library(tidyverse)
@@ -244,12 +247,92 @@ abstracts_sample_sasha <- abstracts_sample %>%
   filter(coder1 == "Sasha" | coder2 == "Sasha" | coder3 == "Sasha")
 
 #write out: saved via Drive, see here: https://drive.google.com/drive/folders/1KSkZW_IoIAjcSiaTGmjHdhZetNV41_l6?usp=drive_link
-#write.csv2(abstracts_sample_daniel, "coding_1_daniel.csv", row.names = F)
-#write.csv2(abstracts_sample_valerie, "coding_1_valerie.csv", row.names = F)
-#write.csv2(abstracts_sample_roksana, "coding_1_roksana.csv", row.names = F)
-#write.csv2(abstracts_sample_samia, "coding_1_samia.csv", row.names = F)
-#write.csv2(abstracts_sample_sjoerd, "coding_1_sjoerd.csv", row.names = F)
-#write.csv2(abstracts_sample_sasha, "coding_1_sasha.csv", row.names = F)
+#write.csv2(abstracts_sample_daniel, "coding_1_sheet_daniel.csv", row.names = F)
+#write.csv2(abstracts_sample_valerie, "coding_1_sheet_valerie.csv", row.names = F)
+#write.csv2(abstracts_sample_roksana, "coding_1_sheet_roksana.csv", row.names = F)
+#write.csv2(abstracts_sample_samia, "coding_1_sheet_samia.csv", row.names = F)
+#write.csv2(abstracts_sample_sjoerd, "coding_1_sheet_sjoerd.csv", row.names = F)
+#write.csv2(abstracts_sample_sasha, "coding_1_sheet_sasha.csv", row.names = F)
 
-#save working space, see here: https://drive.google.com/drive/folders/1KSkZW_IoIAjcSiaTGmjHdhZetNV41_l6?usp=drive_link
-#save.image("abstract_coding.RDATA")
+#### Step 4. Coding 1 (N = 1,002) ####
+
+# N = 1,002 abstracts (N = 167 per year, 2018-2023, everyone coded N = 501 in shuffled teams, majority vote)
+
+##### 4.1 Read in coding jobs and clean #####
+
+abstracts_coding_1 <- read.csv("coding_1_result_sasha.csv")  %>%
+  filter(coder_id == "229") %>%
+  
+  #add codings Valerie
+  rbind(read.csv("coding_1_result_valerie.csv")) %>%
+  
+  #add codings Daniel
+  rbind(read.csv("coding_1_result_daniel.csv") %>%
+          filter(coder_id == "96")) %>%
+  
+  #add codings Roskana
+  rbind(read.csv("coding_1_result_roksana.csv") %>%
+          filter(coder_id == "212")) %>%
+  
+  #add codings Samia
+  rbind(read.csv("coding_1_result_samia.csv") %>%
+          filter(coder_id == "230")) %>%
+  
+  #add codings Sjoerd
+  rbind(read.csv("coding_1_result_sjoerd.csv") %>%
+          filter(coder_id == "224")) %>%
+  
+  #remove id variable
+  filter(unit_id != "id") %>%
+  
+  #harmonize names of coders across coding jobs
+  mutate(coder_name = NA,
+         coder_name = case_when(coder_id == 96 ~ "Daniel",
+                                coder_id %in% c(217) ~ "Valerie",
+                                coder_id %in% c(224) ~ "Sjoerd",
+                                coder_id %in% c(230) ~ "Samia",
+                                coder_id %in% c(212) ~ "Roksana",
+                                coder_id %in% c(229) ~ "Sasha"))
+
+##### 4.2 Harmonize team codes: Majority voting #####
+
+# Create "overall" coding yes/no: based on relevant-1 (= yes) and relevant-2 (= yes)
+abstracts_coding_1 <- abstracts_coding_1 %>%
+  
+  #reduce to necessary variables
+  select(coder_name,  unit_id, variable, value) %>%
+  
+  #to long format
+  pivot_wider(names_from = c("variable"), values_from = "value") %>%
+  
+  #create single measure of inclusion/exclusion
+  mutate(inclusion = 0,
+         inclusion = replace(inclusion,
+                             `relevant-1` == "Yes" & `relevant-2` == "Yes",
+                             1)) %>%
+  
+  #create majority vote
+  group_by(unit_id) %>%
+  mutate(inclusion_team = sum(inclusion),
+         inclusion_team = replace(inclusion_team,
+                                  inclusion_team <=1,
+                                  0),
+         inclusion_team = replace(inclusion_team,
+                                  inclusion_team >=2,
+                                  1)) %>%
+  
+  #keep single observation per unit
+  ungroup() %>%
+  distinct(., unit_id, .keep_all = TRUE) %>%
+  
+  #reduce to necessary variables & bind to abstract data
+  filter(inclusion_team == 1) %>%
+  select(unit_id, inclusion_team) %>%
+  left_join(abstracts %>%
+              rename(unit_id = id)) %>%
+  select(-inclusion_team)
+
+#write.csv2(abstracts_coding_1, "abstracts_coding_1.csv", row.names = F)
+
+#save working space
+#save.image("abstract_coding-1.RDATA")
