c423ba0eae39cfbb55ec0dfec85e33ae008cf49b
thieled <daniel.thiele@fu-berlin.de>
2026-07-01 10:05:19 +0200

    init branch

---
+++ analysis_visualization/scripts/helper_tools2paper.R
@@ -0,0 +1,33 @@
+#############################
+#
+# Title: Merge and aggregate tool annotation to papers
+# Description: Merges and aggregates LLM-annotated (clean) tool names (1.2_tools_annotated)
+# via original names (1.2_tool_dict.csv) on paper-level (2._annotation-results.csv)
+# Helper to be sourced from 2._tables_and_plots.qmd
+#
+# Author: Daniel Thiele
+# Date: 2026-06-30
+#
+#############################
+
+rm(list=ls())
+
+# 00: Prototyping --------------------------------------------------------
+
+# Load tool dictionary: Harmonized tool names
+
+# Load LLM-annotated tool table (unit of obs: tool)
+
+# Load paper table (unit of obs: paper)
+
+
+
+
+# 01: Function -----------------------------------------------------------
+
+
+
+
+
+
+
